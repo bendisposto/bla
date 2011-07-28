3c6c2d41567abddef919756d1743fd75d79ee31a
Jens Bendisposto <jens@bendisposto.de>
2011-07-28 20:23:38 +0200

    project setup

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject b "1.0.0"
+  :description "Interpreter for B"
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dev-dependencies [[swank-clojure "1.3.1"]])
---
+++ test/b/test/core.clj
@@ -0,0 +1,6 @@
+(ns b.test.core
+  (:use [b.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
