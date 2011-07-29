a42943008a41a2be656d3c33aba6e748d405f958
Jens Bendisposto <jens@bendisposto.de>
2011-07-29 11:51:36 +0200

    construct tuples restructured tests

--- src/b/core.clj
+++ src/b/core.clj
@@ -1,24 +1,48 @@
 (ns b.core
 	(:use [clojure.set]))
 
-(declare mk_set contains)
+(declare mk_set mk_tuple contains)
 
-(defn lift [op & p] (fn [e] (apply op ((apply juxt p) e))))
+;((juxt f g) a) = [(f a) (g a)]
+(defn lift [op a b] (fn [e] (apply op ((juxt a b) e)))) 
 
+; Construction
 (defn vrb [x] (fn [e] (e x)))
 (defn intgr [x] (fn [e] x))
 (defn bool [x] (fn [e] x))
 (defn cset [& elements] (apply lift mk_set elements))
+(defn ctuple [a b] (lift mk_tuple a b))
 
-(defn member [e S] (lift contains S e))
+
+;Expressions
 (defn add [a b] (lift + a b))
 (defn sub [a b] (lift - a b))
 (defn mult [a b] (lift * a b))
 (defn div [a b] (lift / a b))
+
 (defn sunion [s1 s2] (lift union s1 s2))
 (defn sintersect [s1 s2] (lift intersection s1 s2))
+(defn ssub [s1 s2] (lift difference s1 s2))
 
+(defn applyfun [fun arg] identity)
+
+; Predicates
+
+(defn grt [a b] (lift > a b))
+(defn grteq [a b] (lift >= a b))
+(defn less [a b] (lift < a b))
+(defn lesseq [a b] (lift <= a b))
+
+(defn member [e S] (lift contains S e))
+
+(defn eq [a b] (lift = a b))
 ;;;;;;;;;
 (defn ev [x] (x {}))
 (defn mk_set [& e] (set e))
+(defn mk_tuple [a b] [a b])
 (defn contains [S e] (not (nil? (S e))))
+
+
+
+; generic version of lift: can lift any number of args
+;(defn lift [op & p] (fn [e] (apply op ((apply juxt p) e))))
--- test/b/test/core.clj
+++ test/b/test/core.clj
@@ -2,10 +2,26 @@
   (:use [b.core])
   (:use [clojure.test]))
 
+(defmacro isnt 
+    [tst & desc] `(is (not ~tst) ~@desc))
+
 (deftest simple-constructs 
   (is (= (ev (intgr 3)) 3) "Simple Integer") 
   (is (= ((vrb :x) {:x 3}) 3) "Variable lookup")
   (is (= (ev (bool true)) true) "Simple Boolean")
+)
+
+(deftest arithmetic_preds 
+  (is (ev (less (intgr 3) (intgr 4)))  "3 < 4") 
+  (isnt (ev (less (intgr 4) (intgr 3)))  "not 4 < 3") 
+  (is (ev (lesseq (intgr 3) (intgr 4)))  "3 <= 4") 
+  (is (ev (lesseq (intgr 3) (intgr 3)))  "3 <= 3") 
+  (isnt (ev (lesseq (intgr 4) (intgr 3)))  "not 4 <= 3") 
+
+  (isnt (ev (grt (intgr 3) (intgr 4)))  "not 3 > 4") 
+  (is (ev (grt (intgr 4) (intgr 3)))  "4 > 3") 
+  (isnt (ev (grteq (intgr 3) (intgr 4)))  "not 3 >= 4") 
+  (is (ev (grteq (intgr 3) (intgr 3)))  "3 >= 3")
 )
 
 (deftest complex-constructs 
@@ -16,14 +32,34 @@
 )
 
 (deftest set-construction
-   (is (= (ev (cset (intgr 1) (intgr 2))) #{1,2}) "integer set construction")
+   (is (= (ev (cset (intgr 1) (intgr 2))) #{1,2}) "integer set construction"))
+
+(deftest set-predicates
    (is (ev (member (intgr 1) (cset (intgr 1) (intgr 2)))) "1 in {1,2}")
-   (is (not (ev (member (intgr 4) (cset (intgr 1) (intgr 2))))) "4 not in {1,2}")
+   (is (not (ev (member (intgr 4) (cset (intgr 1) (intgr 2))))) "4 not in {1,2}"))
+
+(deftest set-expressions
    (is (= (ev (sunion (cset (intgr 1) (intgr 2))
         (cset (intgr 3) (intgr 2)))) #{1,2,3}) "{1,2} U {2,3} = {1,2,3}")
    (is (= (ev (sintersect (cset (intgr 1) (intgr 2))
 	        (cset (intgr 3) (intgr 2)))) #{2}) "{1,2} ^ {2,3} = {2}")
+   (is (= (ev (ssub (cset (intgr 1) (intgr 2))
+			        (cset (intgr 3) (intgr 2)))) #{1}) "{1,2} - {2,3} = {1}")
 )
+
+(deftest tuple-construction
+	(is (= (ev (ctuple (intgr 1) (intgr 2))) [1 2]) "integer tuple construction"))
+	
+(deftest relation-construction
+	(is (= (ev (cset (ctuple (intgr 1) (intgr 1)) (ctuple (intgr 2) (intgr 2))))
+	#{[1,1] [2,2]})
+	"Identity relation on domain 1..2")
+	
+	(is (= (ev (applyfun (cset (ctuple (intgr 1) (intgr 1)) (ctuple (intgr 2) (intgr 2))) 1))
+	1)
+	"apply id function on 1 = 1")
+	
+	)	
 
 
 
