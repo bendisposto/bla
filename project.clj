33cb4c725b135aa83d178d6e36beb1407369ca02
Jens Bendisposto <jens@bendisposto.de>
2011-08-04 09:41:33 +0200

    switch to plain clojure

--- project.clj
+++ project.clj
@@ -6,7 +6,6 @@
 				 [de.prob/parserbase "2.0.58"]               
 				 [de.prob/prologlib "2.0.58"]]
   :dev-dependencies [[swank-clojure "1.3.1"]]
-  :java-source-path "src/java" 
   :main b.core)
 
 
--- src/b/visitor.clj
+++ src/b/visitor.clj
@@ -1,13 +1,23 @@
 (ns b.visitor
-	(:import LispTransformer))
+	(:import de.be4.classicalb.core.parser.analysis.DepthFirstAdapter LispTransformer)
+	)
+
 
 (defn create [start]
 
-    (let [visitor (LispTransformer.)] 
+    (let [sb (StringBuffer.) visitor (
+	       proxy [DepthFirstAdapter] []
+	        (defaultIn [n] (doto sb (.append "(") (.append "b.core/") (.append (.. n (getClass) (getSimpleName)))))
+        	(defaultOut [n] (.append sb ")"))
+            (caseAIntegerExpression [n] (doto sb 
+					(.append "(b.core/AIntegerExpression ") 
+					(.append (.. n (getLiteral) (getText))) 
+				    (.append ")")))
+	)
+	] 
 	    (.apply start visitor)
-	    (.getAst visitor)
+	    (.toString sb)
     )
 		
 		
 )
-
