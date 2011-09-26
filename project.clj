9d6eb142470f19283a30911b00a24ee7b2443bd0
Jens Bendisposto <jens@bendisposto.de>
2011-09-26 17:57:44 +0200

    require core logic

--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
   :description "Interpreter for B"
   :repositories {"cobra" "http://cobra.cs.uni-duesseldorf.de/artifactory/repo/"}
   :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.clojure/core.logic "0.6.4"]
 				 [de.prob/bparser "2.0.58"]
 				 [de.prob/parserbase "2.0.58"]               
 				 [de.prob/prologlib "2.0.58"]]
