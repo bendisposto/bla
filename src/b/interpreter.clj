400620e70394dfa3d54c7427e087a17e450ffea1
Jens Bendisposto <jens@bendisposto.de>
2011-10-02 19:59:48 +0200

    more predicates and expressions

--- src/b/interpreter.clj
+++ src/b/interpreter.clj
@@ -1,40 +1,106 @@
 (ns b.interpreter
    (:gen-class)	
-   (:refer-clojure :exclude [compile])
    (:require [b.reader :as reader])
    (:require [b.typecheck :as type])
+   (:require [clojure.set :as set])
    (:use clojure.algo.monads)
 )
 
+(def wd-state-m (maybe-t state-m))
 				
 (defmacro lift 
-	([name op n]        `(def ~name (with-monad state-m (m-lift ~n ~op))))
+	([name op n]        `(def ~name (with-monad wd-state-m (m-lift ~n ~op))))
 	([name op n & more] `(do (lift ~name ~op ~n) (lift ~@more))))
-	
-(def AIntegerExpression (with-monad state-m m-result))
 
 (defmacro skip 
 	([op] `(defmacro ~op [c#] c#))
 	([op & more] `(do (skip ~op) (skip ~@more))))
 
-
-(lift                             
-	 AAddExpression       +             2
-	 AModuloExpression    mod           2
-	 ADivExpression       (comp int /)  2
-	 AConjunctPredicate   #(and % %2)   2
-	 AEqualPredicate      =             2
-	 AGreaterPredicate    >             2
-	 ALessPredicate       <             2 
-	)                             
-   
-(skip AExpressionParseUnit APredicateParseUnit Start AConvertBoolExpression)
+(skip alpha AExpressionParseUnit APredicateParseUnit Start AConvertBoolExpression)
+(declare bmod bdiv bminus bmult powerset notmember bmin bmax brange bcouple)
 
 (def AIdentifierExpression fetch-val)
+(def AIntegerExpression (with-monad wd-state-m m-result))
+(defn ASetExtensionExpression [& x] (with-monad wd-state-m ((m-lift 1 (partial into #{})) (m-seq x))))
 
-(def compile eval)
+(lift                                                                       
+	 AAddExpression                          +                               2
+	 AModuloExpression                       bmod                            2
+	 ADivExpression                          bdiv                            2
+	 AConjunctPredicate                      #(and % %2)                     2
+	 ADisjunctPredicate                      #(or % %2)                      2
+	 ANegationPredicate                      #(not %)                        1
+	 AEqualPredicate                         =                               2
+	 AUnaryExpression                        (partial * -1)                  1 
+	 AEmptySetExpression                     (constantly #{})                0
+	 AUnionExpression                        set/union                       2        
+	 AIntersectionExpression                 set/intersection                2
+	 AMinusOrSetSubtractExpression           bminus                          2
+	 AMultOrCartExpression                   bmult                           2
+	 APowSubsetExpression                    powerset                        1
+	 ACardExpression                         count                           1
+     ANotBelongPredicate                     notmember                       2
+     AIncludePredicate                       set/subset?                     2
+     AMinExpression                          bmin                            1
+     AMaxExpression                          bmax                            1
+     AIntervalExpression                     brange                          2
+     ALessEqualPredicate                     <=                              2
+  	 AGreaterEqualPredicate                  >=                              2
+     ACoupleExpression                       bcouple                         2
+   	)                             
+
+(defmacro AImplicationPredicate [x y] `(ADisjunctPredicate (ANegationPredicate ~x) ~y))   
+(defmacro AEquivalencePredicate [x y] `(AConjunctPredicate (AImplicationPredicate ~x ~y) (AImplicationPredicate ~y ~x)))
+(defmacro AUnequalPredicate [x y] `(ANegationPredicate (AEqualPredicate ~x ~y)))
+(defmacro APow1SubsetExpression [x] `(AMinusOrSetSubtractExpression (APowSubsetExpression ~x) (APowSubsetExpression (AEmptySetExpression))))
+(defmacro AFinSubsetExpression [x] `(APowSubsetExpression ~x))
+(defmacro AFin1SubsetExpression [x] `(APow1SubsetExpression ~x))
+(defmacro AIncludeStrictlyPredicate [A B] `(AConjunctPredicate (AIncludePredicate ~A ~B) (AUnequalPredicate ~A ~B)))
+(defmacro ANotIncludePredicate [A B] `(ANegationPredicate (AIncludePredicate ~A ~B)))
+(defmacro ANotIncludeStrictlyPredicate [A B] `(ANegationPredicate (AIncludeStrictlyPredicate ~A ~B)))
+(defmacro ABelongPredicate [A B] `(ANegationPredicate (ANotBelongPredicate ~A ~B)))
+(defmacro ALessPredicate [A B] `(ANegationPredicate (AGreaterEqualPredicate ~A ~B)))
+(defmacro AGreaterPredicate [A B] `(ANegationPredicate (ALessEqualPredicate ~A ~B)))
+
+; ------------           MAIN           ------------           
+
 (defn evaluate [env [ast types]] ;(println "evaluate " ast "in" env) 
-         ((compile ast) env))
+                   ((eval ast) env))
 (defn run [text, env]  (->> text reader/parse type/typecheck (evaluate env)))
 (defn -main[ arg]
-    (println (reader/parse arg)))
+    (println (reader/parse arg))) 
+
+; ------------           HELPER           ------------ 
+
+(defn bcouple [a b] [a b])
+
+(defn bmod [n m] 
+	(if (or (< n 0) (<= m 0)) nil (rem n m)))
+	
+(defn bdiv [n m]
+	(if (or (< n 0) (<= m 0)) nil (int (/ n m))))
+	
+(defn bmin [S] (if (and (seq S) (every? number? S)) (apply min S) nil))
+(defn bmax [S] (if (and (seq S) (every? number? S)) (apply max S) nil))	
+	
+(defn bminus [a b]
+	(cond (and (set? a) (set? b)) (set/difference a b)
+	      (and (number? a) (number? b)) (- a b)
+	      :else nil))		
+
+(defn bmult [a b]
+	(cond (and (set? a) (set? b)) (into #{} (for [x a y b] [x y]))
+	      (and (number? a) (number? b)) (* a b)
+	      :else nil))		
+
+(defn powerset [ls]
+    (if (empty? ls) #{#{}}
+        (set/union (powerset (next ls))
+  	    (map #(conj % (first ls)) (powerset (next ls))))))
+
+	
+(defn notmember [e S] (nil? (S e)))	
+(defn member [e S] (not (notmember e S)))
+(defn brange [m n] (into #{} (range m (inc n))))
+
+
--- test/b/test/interpreter.clj
+++ test/b/test/interpreter.clj
@@ -29,20 +29,144 @@
 (isfalse "1+1 > 5")
 (istrue "1=1 & 2>1")
 (isfalse "1=1 & 1+1 > 5")
-
 (istrue "1 < 3")
 (isfalse "11 < 5")
+(istrue "1 = 2 or 1 < 2")
+(istrue "1 = 1 or 4 < 2")
+(isfalse "1 > 1 or 4 < 2")
+(istrue "1 = 1 => 1 < 2")
+(istrue "1 > 1 => 1 < 2")
+(istrue "1 > 1 => 1 = 1")
+(isfalse "1 = 1 => 1 = 2")
+(istrue "not (3 < 2)")
+(isfalse "not (1 < 2)")
+
+(istrue  "(1 = 1) <=> (2 = 2)")
+(istrue  "(1 < 1) <=> (2 > 2)")
+(isfalse "(1 > 1) <=> (2 = 2)")
+(isfalse "(1 = 1) <=> (2 < 2)")
+(istrue "1 /= 2")
+(isfalse "1 /= 1")
+;(istrue "!(x).((x = 1) => (x = 1))") ; Universal quantification
+; Existential quantification 
+
+(istrue "card({1,2,3} * {4,8,3,1,21}) = card({1,2,3}) * card({4,8,3,1,21})")
+(istrue "3 * 5 = 5 * 3")
+(istrue "3 + 5 = 5 + 3")
+(istrue "1 + (2 + 3) = (1 + 2) + 3")
+(istrue "4 * (2 * 3) = (4 * 2) * 3")
+(istrue "5 : {4,5,6,7,1}")
+(isfalse "15 : {4,5,6,7,1}")
+(isfalse "15 : {}")
+
+(isfalse "5 /: {4,5,6,7,1}")
+(istrue "15 /: {4,5,6,7,1}")
+(istrue "15 /: {}")
+
+(istrue "{1,2} <: {1,2}")
+(isfalse "{1,2} <: {1}")
+(istrue "{1,2} <: {1,2,3}")
+(istrue "{2} <: {1,2}")
+(istrue "{} <: {1,2}")
+
+(isfalse "{} /<: {1,2}")
+(istrue "{3} /<: {1,2}")
+
+(istrue "{1,2} <<: {1,2,3}")
+(isfalse "{1,2} <<: {1,2}")
+
+(isfalse "{1,2} /<<: {1,2,3}")
+(istrue "{1,2} /<<: {1,2}")
+
+(istrue "0 <= 4")
+(istrue "0 <= 0")
+(isfalse "55 <= 4")
+
+(istrue "0 >= -2")
+(istrue "4 >= 4")
+(isfalse "3 >= 4")
 
 (check "4" 4)
 (check "1+1" 2)
 (check "4+6" 10)
 (check "4+6+1" 11)
-(check "10 mod 5" 0)
-(check "10 mod 4" 2)
-(check "1 mod 4" 1)
+
 (check "1 / 1" 1)
 (check "2 / 1" 2)
 (check "1 / 2" 0)
+(check "1 / 0" nil)
+(check "-4 / 2" nil)
+(check "4 / -2" nil)
+
+(check "10 mod 5" 0)
+(check "10 mod 4" 2)
+(check "1 mod 4" 1)
+(check "1 mod 2" 1)
+(check "1 mod 0" nil)
+(check "4 mod -2" nil)
+(check "-4 mod 2" nil)
+
+(check "(-4 mod 2) + 14" nil)
+(check "-4 mod 2 + 14" nil)
+(check "{}" #{})
+(check "{1}" #{1})
+(check "{1,2}" #{1,2})
+(check "{1,2,3}" #{1,2,3})
+(check "{1} \\/ {2,3}" #{1,2,3})
+(check "{1} \\/ {}" #{1})
+(check "{} \\/ {1,2}" #{2,1})
+(check "{1} \\/ {1}" #{1})
+(check "{1} \\/ {1,2}" #{1,2})
+(check "{1,3,4} /\\ {1,2,3}" #{1,3})
+(check "{4} /\\ {1,2,3}" #{})
+(check "{} /\\ {1,2,3}" #{})
+(check "1 |-> 2", [1,2])
+(check "{1 |-> 2, 2 |-> 3}", #{[1,2] [2,3]})
+
+(check "{1,2,3,4,5} - {1,3}" #{2,4,5}) ; Set difference
+(check "4-2", 2) ; minus
+(check "4*2", 8) ; multiplication
+(check "{1,2}*{3,4}",#{[1 3] [1 4] [2 3] [2 4]})
+(check "POW({})",#{#{}})
+(check "POW({1})",#{#{}, #{1}})
+(check "POW({1,2})",#{#{}, #{1}, #{2}, #{1,2}})
+(check "POW({1}) - {{}}" #{#{1}})
+
+(check "POW1({})",#{})
+(check "POW1({1})",#{#{1}})
+(check "POW1({1,2})",#{#{1}, #{2}, #{1,2}})
+
+(check "FIN({})",#{#{}})
+(check "FIN({1})",#{#{}, #{1}})
+(check "FIN({1,2})",#{#{}, #{1}, #{2}, #{1,2}})
+(check "FIN({1}) - {{}}" #{#{1}})
+
+(check "FIN1({})",#{})
+(check "FIN1({1})",#{#{1}})
+(check "FIN1({1,2})",#{#{1}, #{2}, #{1,2}})
+
+(check "card({})" 0)
+(check "card({1})" 1)
+(check "card({1,2})" 2)
+(check "card({1,2,3} * {4,8,3,1,21})" (* 3 5))
+
+(check "min({1,2,3,4})", 1)
+(check "min({3,4})", 3)
+(check "min({})" nil)
+
+(check "max({1,2,3,4})", 4)
+(check "max({3,4})", 4)
+(check "max({})" nil)
+
+(check "1..4" #{1,2,3,4})
+(check "0..2" #{0,1,2})
 
 
+(deftest experimente 
+	(is (= (set? #{1,2}) true))
+	(is (= (number? 6) true))
+	(is (= (powerset #{}) #{#{}}))
+	(is (= (powerset #{1}) #{ #{}, #{1}}))
+	(is (= (powerset #{1,2}) #{#{}, #{1}, #{2}, #{1,2}}))
+	)
 
