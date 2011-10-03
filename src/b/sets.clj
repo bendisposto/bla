b63257e074f0179cc94b8c29c1fc2e5a640caaab
Jens Bendisposto <jens@bendisposto.de>
2011-10-03 15:51:26 +0200

    some changes

--- src/b/sets.clj
+++ src/b/sets.clj
@@ -2,44 +2,48 @@
 
 (defprotocol Set
 	 (member? [this e])
-	 (predicate [this]))
-	
+	 (elements [this]))	
 
-(deftype PredicateSet [P] 
+(deftype PredicateSet [P EnumFn] 
 	Set 
-	 (member? [_ e] (P e)))
+	 (member? [_ e] (P e))
+	 (elements [this] (filter P (EnumFn))))
 	
-(deftype IntersectionSet [S1 S2] 
+(deftype IntersectionSet [S1 S2 EnumFn] 
 	Set 
 	 (member? [_ e] (and (member? S1 e) (member? S2 e)))
-	 (predicate [_] (and (predicate S1) (predicate S2))))
+	 (elements [this] (filter #(and (member? S1 %) (member? S2 %)) (EnumFn))))
+	 
 	
-(deftype UnionSet [S1 S2] 
+(deftype UnionSet [S1 S2 EnumFn] 
 	Set 
 	 (member? [_ e] (or (member? S1 e) (member? S2 e)))
- 	 (predicate [_] (or (predicate S1) (predicate S2))))
-	
-(deftype DifferenceSet [S1 S2]  
+	 (elements [this] (filter #(or (member? S1 %) (member? S2 %)) (EnumFn))))
+ 	
+(deftype DifferenceSet [S1 S2 EnumFn]  
 	Set 
 	 (member? [_ e] (and (member? S1 e) (not (member? S2 e))))
-	 (predicate [_] (and (predicate S1) (not (predicate S2)))))
+	 (elements [this] (filter #(and (member? S1 %) (not (member? S2 %))) (EnumFn))))
+	 
 
 (extend-type clojure.lang.PersistentHashSet 
 	Set
 	 (member? [this e] (this e))
-	 (predicate [this] this))
+	 (elements [this] (seq this))
+	 )
 
 (defn enumerate [S Type] (filter (partial member? S) Type))
 
-(def naturals (PredicateSet. #(and (number? %) (<= 0 %))))
-(def integers (PredicateSet. number?))
+(def all-ints (fn [] (interleave (iterate dec 0) (iterate inc 1))))
+
+(def naturals (PredicateSet. #(and (number? %) (<= 0 %)) (fn [] (iterate inc 0))))
+(def integers (PredicateSet. number? all-ints))
 (def bools #{true, false})
-(def strings (PredicateSet. string?))
+(def strings (PredicateSet. string? (fn [] nil)))
 
-(def all-ints (interleave (iterate dec 0) (iterate inc 1)))
 
-(defn mk-set [x] (PredicateSet. x))
-(defn union [x y] (UnionSet. x y))
-(defn intersection [x y] (IntersectionSet. x y))
-(defn difference [x y] (DifferenceSet. x y))
+(defn mk-set [x] (PredicateSet. x nil))
+(defn union [x y] (UnionSet. x y nil))
+(defn intersection [x y] (IntersectionSet. x y nil))
+(defn difference [x y] (DifferenceSet. x y nil))
 
