59fc91bde96a2ac2fcb2b74b6db531899c62791e
Jens Bendisposto <jens@bendisposto.de>
2011-10-03 16:14:34 +0200

    sets as records some first ideas

--- src/b/sets.clj
+++ src/b/sets.clj
@@ -1,49 +1,18 @@
 (ns b.sets (:gen-class))
 
-(defprotocol Set
-	 (member? [this e])
-	 (elements [this]))	
+(def nat-type (iterate inc 0))
+(def int-type (interleave (iterate dec 0) (iterate inc 1)))
+(def bool-type '(false true))
 
-(deftype PredicateSet [P EnumFn] 
-	Set 
-	 (member? [_ e] (P e))
-	 (elements [this] (filter P (EnumFn))))
-	
-(deftype IntersectionSet [S1 S2 EnumFn] 
-	Set 
-	 (member? [_ e] (and (member? S1 e) (member? S2 e)))
-	 (elements [this] (filter #(and (member? S1 %) (member? S2 %)) (EnumFn))))
-	 
-	
-(deftype UnionSet [S1 S2 EnumFn] 
-	Set 
-	 (member? [_ e] (or (member? S1 e) (member? S2 e)))
-	 (elements [this] (filter #(or (member? S1 %) (member? S2 %)) (EnumFn))))
- 	
-(deftype DifferenceSet [S1 S2 EnumFn]  
-	Set 
-	 (member? [_ e] (and (member? S1 e) (not (member? S2 e))))
-	 (elements [this] (filter #(and (member? S1 %) (not (member? S2 %))) (EnumFn))))
-	 
-
-(extend-type clojure.lang.PersistentHashSet 
-	Set
-	 (member? [this e] (this e))
-	 (elements [this] (seq this))
-	 )
-
-(defn enumerate [S Type] (filter (partial member? S) Type))
-
-(def all-ints (fn [] (interleave (iterate dec 0) (iterate inc 1))))
-
-(def naturals (PredicateSet. #(and (number? %) (<= 0 %)) (fn [] (iterate inc 0))))
-(def integers (PredicateSet. number? all-ints))
-(def bools #{true, false})
-(def strings (PredicateSet. string? (fn [] nil)))
+(def printable-chars (map char (range 32 126)))
 
 
-(defn mk-set [x] (PredicateSet. x nil))
-(defn union [x y] (UnionSet. x y nil))
-(defn intersection [x y] (IntersectionSet. x y nil))
-(defn difference [x y] (DifferenceSet. x y nil))
+(defn make-deferred-type [name]  (for [p (repeat name) n (iterate inc 1)] (str p n)))
+(defn make explicit-type [& elements] (into #{} elements))
+
+
+(defn mk-set [x] #{})
+(defn union [x y] x)
+(defn intersection [x y] x)
+(defn difference [x y] x)
 
