a2554c1e1f76293a8e1c9fedba6d7307f8bbf406
Jens Bendisposto <jens@bendisposto.de>
2011-10-04 17:02:01 +0200

    more work on sets, it's getting spooky

--- src/b/sets.clj
+++ src/b/sets.clj
@@ -1,9 +1,9 @@
-(ns b.sets (:gen-class))
+(ns b.sets 
+	(:require [clojure.core.match.core :as match])
+	(:gen-class))
 
 (defrecord B-Set [type accept? reject? precise])
 
-
-(def nat-type (iterate inc 0))
 (def int-type (interleave (iterate dec 0) (iterate inc 1)))
 (def bool-type '(false true))
 (def printable-chars (map char (range 32 126)))
@@ -11,20 +11,23 @@
 (defn make-deferred-type [name]  (for [p (repeat name) n (iterate inc 1)] (str p n)))
 (defn make-explicit-type [& elements] (into #{} elements))
 
-(def nat (B-Set. (fn [] nat-type) (or pos? zero?) neg? #{:accept? :reject?}))
-(def snat (B-Set. (fn [] nat-type) (constantly false) neg? #{:reject?}))
+(def nat (B-Set. (fn [] int-type) #(or (pos? %) (zero? %)) neg? #{:accept? :reject?}))
+
+(defn- mk-filter [accept? reject? precise]
+        (match/match [(boolean (:accept? precise))
+                      (boolean (:reject? precise))]
+	         [true   true] #(and (accept? %) (not (reject? %)))
+	         [true  false] accept?
+	         [false  true] (comp not reject?)
+	         [false false] #(or (accept? %) (not (reject? %)))))
 
 (defn member? [e {:keys [type accept? reject? precise]}]
+;	(println "member?" e " in type" type "acc:" accept? (accept? e) "rej:" reject? (reject? e))
 	(cond (accept? e) true
 	      (reject? e) false
-	       :else 
-	         (let [enumerator (type) 
-		           ap (:accept? precise)
-	               rp (:reject? precise)
-	               filterp (cond (and ap rp) #(and accept? (comp not reject?)) 
-	                              ap accept?
-	                              rp (comp not reject?))]
-	                   (some #(= e %) (filter filterp enumerator)))))
+	       :else (some #(= e %) 
+	                   (do ;(println "enumerate") 
+		                   (filter (mk-filter accept? reject? precise) (type))))))
 
 (defn mk-set [x] #{})
 (defn union [x y] x)
--- test/b/test/sets.clj
+++ test/b/test/sets.clj
@@ -1,4 +1,31 @@
 (ns b.test.sets
   (:use [b.sets])
+  (:import b.sets.B-Set)
   (:use [clojure.test]))
+
+(defmacro ignore [_ _])
+
+(def semi-reject-nat (B-Set. (fn [] int-type) (constantly false) neg? #{:reject?}))
+(def semi-accept-nat (B-Set. (fn [] int-type) 
+#(or (pos? %) (zero? %)) 
+(constantly false) 
+#{:accept?}))
+(def weird-nat (B-Set. (fn [] int-type) 
+                       #(and (even? %) (or (pos? %) (zero? %))) 
+                       #(or (not (odd? %)) (neg? %)) 
+                       #{}))
+
+(deftest membertest
+	(is (member? 1 nat))
+	(is (member? 0 nat))
+	(is (not (member? -1 nat)))
+	(is (member? 1 semi-reject-nat))
+	(is (member? 0 semi-reject-nat))
+	(is (not (member? -1 semi-reject-nat)))
+	(is (member? 1 semi-accept-nat))
+	(is (member? 0 semi-accept-nat))
+	(ignore "infinite loop because of semi-decision" (is (not (member? -1 semi-accept-nat))))
+	(is (member? 1 weird-nat))
+	(is (member? 0 weird-nat))
+	(is (not (member? -1 weird-nat))))
 
