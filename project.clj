d2434cac34da9a28713533bc7a2ee4d87209f202
Jens Bendisposto <jens@bendisposto.de>
2011-11-02 23:32:35 +0100

    midje is actually a dev dependency. Also removed obsolete dependencies

--- project.clj
+++ project.clj
@@ -2,16 +2,12 @@
   :description "B Lisp Animator, an Interpreter for the B-Method in Clojure"
   :repositories {"cobra" "http://cobra.cs.uni-duesseldorf.de/artifactory/repo/"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 ;[org.clojure/core.logic "0.6.4"]
-                 ;[org.clojure/math.combinatorics "0.0.1"]
-                 [midje "1.3-alpha4"]
                  [org.clojure/algo.monads "0.1.0"]
-                 [org.clojure/tools.trace "0.7.1"]
- ;               [org.clojure/core.match "0.2.0-alpha3"]
 		 [de.prob/bparser "2.0.58"]
 		 [de.prob/parserbase "2.0.58"]               
 		 [de.prob/prologlib "2.0.58"]]
   :dev-dependencies [[swank-clojure "1.3.3"]
+                     [midje "1.3-alpha4"]
                      [lein-midje "1.0.4"]
                      [lein-eclipse "1.0.0"]
                      [marginalia "0.7.0-SNAPSHOT"]
