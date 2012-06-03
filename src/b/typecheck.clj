d38b6b2f5abb7a38c9678c6ce9ad27a8e3af8fb7
Jens Bendisposto <jens@bendisposto.de>
2012-06-03 10:39:53 +0200

    more work on typechecker

--- src/b/typecheck.clj
+++ src/b/typecheck.clj
@@ -1,6 +1,11 @@
 (ns b.typecheck 
   (:refer-clojure :exclude [==])
-  (:use clojure.core.logic)) 
+  (:use clojure.core.logic))
+
+
+;(defn binopo [btw vars t1 t2 rt]
+;  (f)
+;)
 
 (defmacro rule [n vars t1 t2 rt]
   `(defn ~n [a# b#]
@@ -15,24 +20,59 @@
   ([n v a b c & more] `(do (rule ~n ~v ~a ~b ~c) (rules ~@more)))
   ([n v a b c] `(rule ~n ~v ~a ~b ~c)))
 
+(defmacro junctor
+  ([n] `(rule ~n [] [:bool] [:bool] [:bool]))
+  ([n & more] `(do (junctor ~n) (junctor ~@more))))
+
+(defmacro arithmetic
+  ([n] `(rule ~n [] [:int] [:int] [:int]))
+  ([n & more] `(do (arithmetic ~n) (arithmetic ~@more))))
+
+(defmacro arithmetic_predicate
+  ([n] `(rule ~n [] [:int] [:int] [:bool]))
+  ([n & more] `(do (arithmetic_predicate ~n) (arithmetic_predicate ~@more))))
+
+
+(junctor AConjunctPredicate ADisjunctPredicate AImplicationPredicate AEquivalencePredicate)
+(arithmetic AAddExpression)
+(arithmetic_predicate ALessPredicate ALessEqualPredicate AGreaterPredicate AGreaterEqualPredicate)
+
 
 (rules
- ;name              type variables   left type   right type   result type
- ABelongPredicate   [t]              t           [:set t]     [:bool]
- AIncludesPredicate [t]              [:set t]    [:set t]     [:bool]
- AAddExpression     []               [:int]      [:int]       [:int]
- )
+ ;name                         type variables   left type   right type   result type
+ AMemberPredicate              [t]              t           [:set t]     [:bool]
+ ASubsetPredicate              [t]              [:set t]    [:set t]     [:bool]
+ AEqualPredicate               [t]              t           t            [:bool]
+ AMinusOrSetSubtractExpression [t]              t           t            t
+)
 
+
+;(defn AMultOrCartExpression [a b] (fn [type in out] (conde ())  ))
+
+
+(def ASubsetStrictPredicate ASubsetPredicate)
+(def ANotSubsetStrictPredicate ASubsetPredicate)
+(def ANotSubsetPredicate ASubsetPredicate)
+
+(defmacro AForallPredicate [_ p] `(~p))
+(defmacro AExistsPredicate [_ p] `(~p))
+
+(def ANotEqualPrdicate AEqualPredicate)
+(def ANotMemberPredicate AMemberPredicate)
+
+(defn AFinitePredicate [e] (fn [type in out] (fresh [t] (== type [:set t]) (e type in out))))
+(defn ANegationPredicate [_] (fn [type in out] (all (== type [:bool]) (== in out))))
 (defn AIntegerExpression [_] (fn [type in out] (all (== type [:int]) (== in out))))
-(defn AIdentifierExpression [id] (fn [type in out] (appendo in [[id type]] out)))
+(defn AIdentifierExpression [id]
+  (fn [type in out]
+    (fresh [t]
+           (conda ((membero [id t] in) (== t type) (== in out))
+                  ((appendo in [[id type]] out))))))
 (defn ANatSetExpression [] (fn [type in out] (all (== type [:set [:int]]) (== in out))))
 
 (defn typecheck [ast] (let [res (first (run* [q] (fresh [t,o] (ast t [] q))))] (into {} res)))
 
-(defn t1 [] (run* [q] (fresh [t,o] ((AAddExpression (AIntegerExpression 4) (AIdentifierExpression :x)) t [] o) (== q {:type t, :env o}))))
-(defn t2 [] (typecheck (AAddExpression (AIntegerExpression 4) (AIdentifierExpression :x))))
-(defn t3 [] (typecheck (ABelongPredicate (AIdentifierExpression :x) (ANatSetExpression))))
-(defn t4 [] (typecheck (ABelongPredicate (AIdentifierExpression :e) (AIdentifierExpression :S))))
+
 
 
 
--- test/b/test/typecheck.clj
+++ test/b/test/typecheck.clj
@@ -3,38 +3,12 @@
   (:use [b.typecheck])
   (:use [midje.sweet]))
 
-(fact "Storing in the environment"
-  (store :x [:int] {}) => {:x [:int]}
-  (store :x [:int] {:x [:int]})  => {:x [:int]}
-  (store :x [:int] {:x [:bool]})  => nil
-  (store :x [:int] {:y [:int]})  => {:x [:int] :y [:int]}
-  (store :x [:set [:int]] {:x [:set ['?x]]}) => {:x [:set [:int]]})
-
-(defn consistent-type? [id] (fn [[x e]] (= x (e id))))
-
-(fact "Atomic types"
- ((AIntegerExpression 17) {}) => [[:int] {}]
- ((AIdentifierExpression :x) {:x [:foo]}) => [[:foo] {:x [:foo]}]
-)
-
-(fact "constent checker"
-  ((consistent-type? :x) [:foo {:x :bar}]) => falsey
-  ((consistent-type? :x) [:bar {:x :bar}]) => truthy
-)
-
 (fact
-  [:int {:x :int}] => (consistent-type? :x)
-  ((AIdentifierExpression :x) {:y [:foo]}) => (consistent-type? :x)
-  ((AIdentifierExpression :x) {}) => (consistent-type? :x))
-
-;.;. FAIL at (NO_SOURCE_FILE:1)
-;.;.     Expected: [[:int] {:x [:int]}]
-;.;.       Actual: clojure.lang.ArityException: Wrong number of args (0) passed to: typecheck$AIntegerExpression$fn
-;.;.               b.typecheck$AAddExpression$fn__5638.invoke(typecheck.clj:21)
-;.;.               b.test.typecheck$eval5743$fn__5744$fn__5745.invoke(NO_SOURCE_FILE:1)
-;.;.               b.test.typecheck$eval5743$fn__5744.invoke(NO_SOURCE_FILE:1)
-;.;.               b.test.typecheck$eval5743.invoke(NO_SOURCE_FILE:1)
-;.;.               b.test.typecheck$eval5739.invoke(NO_SOURCE_FILE)
-(fact
-  ((AAddExpression (AIntegerExpression 1) (AIdentifierExpression :x)) {}) => [[:int] {:x [:int]}])
+  (typecheck (AAddExpression (AIntegerExpression 4) (AIdentifierExpression :x))) => {:x [:int]}
+  (:x (typecheck (AMemberPredicate (AIdentifierExpression :x) (ANatSetExpression)))) => [:int]
+  (:x (typecheck (ASubsetPredicate (AIdentifierExpression :x) (ANatSetExpression)))) => [:set [:int]]
+  (:x (typecheck (AEqualPredicate (AIdentifierExpression :x) (ANatSetExpression)))) => [:set [:int]]
+  (:x (typecheck (AEqualPredicate (AIdentifierExpression :x) (AIntegerExpression 1)))) => [:int]
+  (:x (typecheck (AConjunctPredicate (AEqualPredicate (AIdentifierExpression :x) (AIdentifierExpression :y)) (AEqualPredicate (AIdentifierExpression :y) (AIntegerExpression 1))))) => [:int]
+  )
 
