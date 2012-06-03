b523c14c9a23af30bd5bea59b294555471ce7ed3
Jens Bendisposto <jens@bendisposto.de>
2012-06-03 17:48:37 +0200

    more typechecking

--- src/b/typecheck.clj
+++ src/b/typecheck.clj
@@ -3,10 +3,6 @@
   (:use clojure.core.logic))
 
 
-;(defn binopo [btw vars t1 t2 rt]
-;  (f)
-;)
-
 (defmacro rule [n vars t1 t2 rt]
   `(defn ~n [a# b#]
      (fn [t# in# out#]
@@ -20,22 +16,9 @@
   ([n v a b c & more] `(do (rule ~n ~v ~a ~b ~c) (rules ~@more)))
   ([n v a b c] `(rule ~n ~v ~a ~b ~c)))
 
-(defmacro junctor
-  ([n] `(rule ~n [] [:bool] [:bool] [:bool]))
-  ([n & more] `(do (junctor ~n) (junctor ~@more))))
-
-(defmacro arithmetic
-  ([n] `(rule ~n [] [:int] [:int] [:int]))
-  ([n & more] `(do (arithmetic ~n) (arithmetic ~@more))))
-
-(defmacro arithmetic_predicate
-  ([n] `(rule ~n [] [:int] [:int] [:bool]))
-  ([n & more] `(do (arithmetic_predicate ~n) (arithmetic_predicate ~@more))))
-
-
-(junctor AConjunctPredicate ADisjunctPredicate AImplicationPredicate AEquivalencePredicate)
-(arithmetic AAddExpression)
-(arithmetic_predicate ALessPredicate ALessEqualPredicate AGreaterPredicate AGreaterEqualPredicate)
+(defmacro analog
+  ([f g] `(def ~g ~f))
+  ([f g & more] `(do (analog ~f ~g) (analog ~f ~@more))))
 
 
 (rules
@@ -44,31 +27,61 @@
  ASubsetPredicate              [t]              [:set t]    [:set t]     [:bool]
  AEqualPredicate               [t]              t           t            [:bool]
  AMinusOrSetSubtractExpression [t]              t           t            t
+ AConjunctPredicate            []               [:bool]     [:bool]      [:bool]
+ AAddExpression                []               [:int]      [:int]       [:int]
+ ALessPredicate                []               [:int]      [:int]       [:bool]
+ AIntervalExpression           []               [:int]      [:int]       [:set [:int]]
+ AUnionExpression              [t]              [:set t]    [:set t]     [:set t]
 )
 
+(analog AConjunctPredicate ADisjunctPredicate AImplicationPredicate AEquivalencePredicate)
+(analog AAddExpression ADivExpression AModExpression APowerOfExpression)
+(analog ALessPredicate ALessEqualPredicate AGreaterPredicate AGreaterEqualPredicate)
+(analog ASubsetPredicate ASubsetStrictPredicate ANotSubsetStrictPredicate ANotSubsetPredicate)
+(analog AEqualPredicate ANotEqualPrdicate)
+(analog AMemberPredicate ANotMemberPredicate)
+(analog AUnionExpression AIntersectionExpresson ASetSubtractionExpression)
 
-;(defn AMultOrCartExpression [a b] (fn [type in out] (conde ())  ))
+
+(defn AMultOrCartExpression [a b] 
+	(fn [type in out] 
+		(fresh [btw s t]
+			   (conde ((a [:int] in btw)   (b [:int] btw out) (== type [:int]))
+		              ((a [:set s] in btw) (b [:set t] btw out) (== type [:cart s t]))))))
 
 
-(def ASubsetStrictPredicate ASubsetPredicate)
-(def ANotSubsetStrictPredicate ASubsetPredicate)
-(def ANotSubsetPredicate ASubsetPredicate)
 
+(defmacro AConvertBoolExpression [p] `(~p))
 (defmacro AForallPredicate [_ p] `(~p))
 (defmacro AExistsPredicate [_ p] `(~p))
 
-(def ANotEqualPrdicate AEqualPredicate)
-(def ANotMemberPredicate AMemberPredicate)
+(defn APowSubsetExpression [e] (fn [type in out]  (fresh [t] (e t in out) (== type [:set t]))))
+(analog APowSubsetExpression  APow1SubsetExpression AFinSubsetExpression AFin1SubsetExpression)
+
+(defn identity [e] (fn [type in out] (fresh [t] (e t in out) (== type [:cart t t]))))
 
 (defn AFinitePredicate [e] (fn [type in out] (fresh [t] (== type [:set t]) (e type in out))))
 (defn ANegationPredicate [_] (fn [type in out] (all (== type [:bool]) (== in out))))
+
 (defn AIntegerExpression [_] (fn [type in out] (all (== type [:int]) (== in out))))
+(analog AIntegerExpression AUnaryMinusExpression)
+
+(defn ACardExpression [e] (fn [type in out] (fresh [t] (== type [:int]) (e [:set t] in out))))
+(defn AMaxExpression [e] (fn [type in out] (all (== type [:int]) (e [:set [:int]] in out))))
+(analog AMaxExpression AMinExpression)
+
+
+
 (defn AIdentifierExpression [id]
   (fn [type in out]
     (fresh [t]
            (conda ((membero [id t] in) (== t type) (== in out))
                   ((appendo in [[id type]] out))))))
+
 (defn ANatSetExpression [] (fn [type in out] (all (== type [:set [:int]]) (== in out))))
+(defn ASuccessorExpression [] (fn [type in out] (all (== type [:cart [:int] [:int]]) (== in out))))
+(analog ASuccessorExpression APredecessorExpression)
+
 
 (defn typecheck [ast] (let [res (first (run* [q] (fresh [t,o] (ast t [] q))))] (into {} res)))
 
--- test/b/test/typecheck.clj
+++ test/b/test/typecheck.clj
@@ -10,5 +10,7 @@
   (:x (typecheck (AEqualPredicate (AIdentifierExpression :x) (ANatSetExpression)))) => [:set [:int]]
   (:x (typecheck (AEqualPredicate (AIdentifierExpression :x) (AIntegerExpression 1)))) => [:int]
   (:x (typecheck (AConjunctPredicate (AEqualPredicate (AIdentifierExpression :x) (AIdentifierExpression :y)) (AEqualPredicate (AIdentifierExpression :y) (AIntegerExpression 1))))) => [:int]
+  (:x (typecheck (AMultOrCartExpression (AIdentifierExpression :x) (AIntegerExpression 1)))) => [:int]
+  (first (:y (typecheck (AEqualPredicate (AIdentifierExpression :y) (AMultOrCartExpression (AIdentifierExpression :x) (ANatSetExpression)))))) => :cart
   )
 
