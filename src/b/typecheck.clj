ec4a5dfe5039ad3a21a94cb734620910b618c255
Jens Bendisposto <jens@bendisposto.de>
2012-06-16 17:47:12 +0200

    tests !!! a couple of bugs found and fixed

--- src/b/typecheck.clj
+++ src/b/typecheck.clj
@@ -3,7 +3,7 @@
   (:use clojure.core.logic))
 
 
-(defmacro rule [n vars t1 t2 rt]
+(defmacro binary_rule [n vars [t1 t2] rt]
   `(defn ~n [a# b#]
      (fn [t# in# out#]
        (fresh [btw#]
@@ -12,65 +12,154 @@
                      (b# ~t2 btw# out#)
                      (== t# ~rt))))))
 
+
+(defmacro unary_rule [n vars t rt]
+  `(defn ~n [a#]
+     (fn [t# in# out#]
+            (fresh ~vars
+                     (a# ~t in# out#)
+                     (== t# ~rt)))))
+
+(defmacro constant_rule [n rt]
+   `(defn ~n []
+        (fn [t# in# out#]
+               (all
+                        (== in# out#)
+                        (== t# ~rt)))))
+
 (defmacro rules
-  ([n v a b c & more] `(do (rule ~n ~v ~a ~b ~c) (rules ~@more)))
-  ([n v a b c] `(rule ~n ~v ~a ~b ~c)))
+  ([n ar v at rt & more] `(do (rules ~n ~ar ~v ~at ~rt) (rules ~@more)))
+  ([n ar v at rt] (cond 
+	               (= ar 0) `(constant_rule ~n ~rt)
+	               (= ar 1) `(unary_rule ~n ~v ~(first at) ~rt)
+	               (= ar 2) `(binary_rule ~n ~v ~at ~rt))))
 
 (defmacro analog
   ([f g] `(def ~g ~f))
   ([f g & more] `(do (analog ~f ~g) (analog ~f ~@more))))
 
 
-(rules
- ;name                         type variables   left type   right type   result type
- AMemberPredicate              [t]              t           [:set t]     [:bool]
- ASubsetPredicate              [t]              [:set t]    [:set t]     [:bool]
- AEqualPredicate               [t]              t           t            [:bool]
- AMinusOrSetSubtractExpression [t]              t           t            t
- AConjunctPredicate            []               [:bool]     [:bool]      [:bool]
- AAddExpression                []               [:int]      [:int]       [:int]
- ALessPredicate                []               [:int]      [:int]       [:bool]
- AIntervalExpression           []               [:int]      [:int]       [:set [:int]]
- AUnionExpression              [t]              [:set t]    [:set t]     [:set t]
-)
+(defmacro skip 
+	([op] `(defmacro ~op [c#] c#))
+	([op & more] `(do (skip ~op) (skip ~@more))))
 
+
+(skip AExpressionParseUnit APredicateParseUnit Start AConvertBoolExpression)
+
+(defmacro bint [] `:integer)
+(defmacro bbool [] `:boolean)
+(defmacro bstring [] `:string)
+(defmacro bset [t] `[:set ~t])
+(defmacro bpair [s t] `[:pair ~s ~t])
+
+(defmacro brel [s t] `(bset (bpair ~s ~t)))
+(defmacro bseq [t] `(brel (bint) ~t))
+
+
+
+
+(rules
+ ;name                           arity     type variables   argument types                      result type
+ ;                                                            first         second       
+ AMemberPredicate                2         [t]              [ t             (bset t)      ]     (bbool)
+ ASubsetPredicate                2         [t]              [ (bset t)      (bset t)      ]     (bbool)
+ AEqualPredicate                 2         [t]              [ t             t             ]     (bbool)
+ AMinusOrSetSubtractExpression   2         [t]              [ t             t             ]     t
+ AConjunctPredicate              2         []               [ (bbool)       (bbool)       ]     (bbool)
+ AAddExpression                  2         []               [ (bint)        (bint)        ]     (bint)
+ ALessPredicate                  2         []               [ (bint)        (bint)        ]     (bbool)
+ AIntervalExpression             2         []               [ (bint)        (bint)        ]     (bset (bint))
+ AUnionExpression                2         [t]              [ (bset t)      (bset t)      ]     (bset t)
+ APowSubsetExpression            1         [t]              [ (bset t)                    ]     (bset (bset t))
+ AIdentityExpression             1         [t]              [ (bset t)                    ]     (brel t t)
+ ANegationPredicate              1         []               [(bbool)                      ]     (bbool)
+ ACardExpression                 1         [t]              [(bset t)                     ]     (bint)
+ AMaxExpression                  1         []               [(bset (bint))                ]     (bint)
+ ADomainExpression               1         [s,t]            [(brel s t)                   ]     (bset s)
+ ARangeExpression                1         [s,t]            [(brel s t)                   ]     (bset t)
+ ADomainRestrictionExpression    2         [s,t]            [(bset s)       (brel s t)    ]     (brel s t)
+ ARangeRestrictionExpression     2         [s,t]            [(brel s t)     (bset t)       ]     (brel s t) 
+ AOverwriteExpression            2         [s,t]            [(brel s t)    (brel s t)   ]     (brel s t)
+ ARelationsExpression            2         [s,t]            [(bset s)       (bset t)      ]     (brel s t)
+ AImageExpression                2         [s,t]            [(brel s t)     (bset s)     ]     (bset t)
+ AClosureExpression              1         [t]              [(brel t t)                   ]     (brel t t)
+ AIterationExpression              2         [t]              [(brel t t)      (bint)       ]     (brel t t)
+ ACompositionExpression          2         [r,s,t]          [(brel r s)      (brel s t)   ]     (brel r t)
+ ASeqExpression                  1         [t]              [(bset t)                     ]     (bseq t)
+ ANatSetExpression               0         []               [                             ]     (bset (bint))
+ ASuccessorExpression            0         []               [                             ]     (bseq (bint))
+ AFirstExpression                1         [t]              [(bseq t)                     ]     t
+ AFrontExpression                1         [t]              [(bseq t)                     ]     (bseq t)
+ AConcatExpression               2         [t]              [(bseq t)       (bseq t)      ]     (bseq t)
+ AReverseExpression              1         [s,t]            [(brel s t)                    ]     (brel t s)
+ ABooleanTrueExpression          0         []               [                             ]     (bbool)
+ ABoolSetExpression              0         []               [                             ]     (bset (bbool))
+ AStringSetExpression            0         []               [                             ]     (bset (bstring))
+ AInsertFrontExpression          2         [t]              [t              (bseq t)      ]     (bseq t)
+ AInsertTailExpression           2         [t]              [(bseq t)       t             ]     (bseq t)
+ ARestrictFrontExpression        2         [t]              [(bseq t)       (bint)        ]     (bseq t)
+ singleton_set                   1         [t]              [t                            ]     (bset t)
+ ACoupleExpression               2         [s t]            [s              t             ]     (bpair s t)
+)                                                                                       
+(defmacro leaf 
+  ([n t] `(defn ~n [value#] (fn [type# in# out#] (all (== type# ~t) (== in# out#)))))
+  ([n t & more] `(do (leaf ~n ~t) (leaf ~@more))))
+
+(leaf AIntegerExpression (bint) AStringExpression (bstring))
+
+(analog ANatSetExpression ANat1SetExpression ANaturalSetExpression ANatural1SetExpression AIntSetExpression AIntegerSetExpression)
+
+(analog AIntegerExpression AMaxIntExpression AMinIntExpression )
+(analog ABooleanTrueExpression ABooleanFalseExpression)
+
+(defn AEmptySequenceExpression [] (fn [type in out] (fresh [t] (== in out) (== type (bseq t)))))
+(defn AEmptySetExpression [] (fn [type in out] (fresh [t] (== in out) (== type (bset t)))))
+
+
+(analog ARestrictFrontExpression ARestrictTailExpression)
+
+(analog AIntegerExpression AUnaryMinusExpression)
+(analog ADomainRestriction ADomainSubtraction)
+(analog ARangeRestriction ARangeSubtraction)
 (analog AConjunctPredicate ADisjunctPredicate AImplicationPredicate AEquivalencePredicate)
 (analog AAddExpression ADivExpression AModExpression APowerOfExpression)
 (analog ALessPredicate ALessEqualPredicate AGreaterPredicate AGreaterEqualPredicate)
 (analog ASubsetPredicate ASubsetStrictPredicate ANotSubsetStrictPredicate ANotSubsetPredicate)
-(analog AEqualPredicate ANotEqualPrdicate)
+(analog AEqualPredicate ANotEqualPredicate)
 (analog AMemberPredicate ANotMemberPredicate)
-(analog AUnionExpression AIntersectionExpresson ASetSubtractionExpression)
+(analog AUnionExpression AIntersectionExpression ASetSubtractionExpression)
+(analog APowSubsetExpression  APow1SubsetExpression AFinSubsetExpression AFin1SubsetExpression)
+(analog AMaxExpression AMinExpression)
+(analog ARelationsExpression APartialFunctionExpression ATotalFunctionExpression APartialInjectionExpression
+        ATotalInjectionExpression APartialSurjectionExpression ATotalSurjectionExpression
+        APartialBijectionExpression ATotalBijectionExpression)
+(analog AClosureExpression AReflexiveClosureExpression)
+(analog ASeqExpression ASeq1Expression AISeqExpression AISeq1Expression APermExpression) 
+(analog ASuccessorExpression APredecessorExpression)
+(analog ACardExpression ASizeExpression)
+(analog AFirstExpression ALastExpression)
+(analog AFrontExpression ATailExpression ARevExpression)
+
+
+
+(defmacro ASetExtensionExpression
+  ([] `(AEmptySetExpression))
+  ([e & es] `(AUnionExpression (singleton_set ~e)  (ASetExtensionExpression ~@es))))
 
 
 (defn AMultOrCartExpression [a b] 
-	(fn [type in out] 
-		(fresh [btw s t]
-			   (conde ((a [:int] in btw)   (b [:int] btw out) (== type [:int]))
-		              ((a [:set s] in btw) (b [:set t] btw out) (== type [:cart s t]))))))
+  (fn [type in out] 
+    (fresh [btw s t]
+           (conde ((a (bint) in btw)   (b (bint) btw out) (== type (bint)))
+                  ((a (bset s) in btw) (b (bset t) btw out) (== type (brel s t)))))))
 
 
-
-(defmacro AConvertBoolExpression [p] `(~p))
-(defmacro AForallPredicate [_ p] `(~p))
-(defmacro AExistsPredicate [_ p] `(~p))
-
-(defn APowSubsetExpression [e] (fn [type in out]  (fresh [t] (e t in out) (== type [:set t]))))
-(analog APowSubsetExpression  APow1SubsetExpression AFinSubsetExpression AFin1SubsetExpression)
-
-(defn identity [e] (fn [type in out] (fresh [t] (e t in out) (== type [:cart t t]))))
-
-(defn AFinitePredicate [e] (fn [type in out] (fresh [t] (== type [:set t]) (e type in out))))
-(defn ANegationPredicate [_] (fn [type in out] (all (== type [:bool]) (== in out))))
-
-(defn AIntegerExpression [_] (fn [type in out] (all (== type [:int]) (== in out))))
-(analog AIntegerExpression AUnaryMinusExpression)
-
-(defn ACardExpression [e] (fn [type in out] (fresh [t] (== type [:int]) (e [:set t] in out))))
-(defn AMaxExpression [e] (fn [type in out] (all (== type [:int]) (e [:set [:int]] in out))))
-(analog AMaxExpression AMinExpression)
+(defmacro AForallPredicate [_ p] `~p)
+(defmacro AExistsPredicate [_ p] `~p)
 
 
+(defn RLambdaExpression [ex pred & vars] (let [vs (reverse vars)] (fn [type in out]    )))
+(defn ALambdaExpression [& args] (apply RLambdaExpression (reverse args)))
 
 (defn AIdentifierExpression [id]
   (fn [type in out]
@@ -78,27 +167,6 @@
            (conda ((membero [id t] in) (== t type) (== in out))
                   ((appendo in [[id type]] out))))))
 
-(defn ANatSetExpression [] (fn [type in out] (all (== type [:set [:int]]) (== in out))))
-(defn ASuccessorExpression [] (fn [type in out] (all (== type [:cart [:int] [:int]]) (== in out))))
-(analog ASuccessorExpression APredecessorExpression)
-
-
 (defn typecheck [ast] (let [res (first (run* [q] (fresh [t,o] (ast t [] q))))] (into {} res)))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
--- test/b/test/typecheck.clj
+++ test/b/test/typecheck.clj
@@ -1,16 +1,81 @@
 (ns b.test.typecheck
+  (:require [b.reader :as reader])
   (:use [clojure.test])
   (:use [b.typecheck])
   (:use [midje.sweet]))
 
-(fact
-  (typecheck (AAddExpression (AIntegerExpression 4) (AIdentifierExpression :x))) => {:x [:int]}
-  (:x (typecheck (AMemberPredicate (AIdentifierExpression :x) (ANatSetExpression)))) => [:int]
-  (:x (typecheck (ASubsetPredicate (AIdentifierExpression :x) (ANatSetExpression)))) => [:set [:int]]
-  (:x (typecheck (AEqualPredicate (AIdentifierExpression :x) (ANatSetExpression)))) => [:set [:int]]
-  (:x (typecheck (AEqualPredicate (AIdentifierExpression :x) (AIntegerExpression 1)))) => [:int]
-  (:x (typecheck (AConjunctPredicate (AEqualPredicate (AIdentifierExpression :x) (AIdentifierExpression :y)) (AEqualPredicate (AIdentifierExpression :y) (AIntegerExpression 1))))) => [:int]
-  (:x (typecheck (AMultOrCartExpression (AIdentifierExpression :x) (AIntegerExpression 1)))) => [:int]
-  (first (:y (typecheck (AEqualPredicate (AIdentifierExpression :y) (AMultOrCartExpression (AIdentifierExpression :x) (ANatSetExpression)))))) => :cart
-  )
+(defn formulize [text] (str "#FORMULA " text))
+(defn tc [text] (->> text formulize reader/parse typecheck))
+(defn dg [f] (->> f formulize reader/bparser reader/mk_clojure_ast))
+
+;.;. The highest reward for a man's toil is not what he gets for it but
+;.;. what he becomes by it. -- Ruskin
+(tabular "basic-rules"
+         (fact (tc ?text) => (just ?out))
+         ?text | ?out
+         "c : STRING" | {:c (bstring)} 
+         "1 : NAT" | {}
+         "x <: NAT" | {:x (bset (bint))}
+         "x <: BOOL" | {:x (bset (bbool))}         
+         "3+x" | {:x (bint)}
+         "x + y" | {:y (bint), :x (bint)}
+         "1 < y" | {:y (bint)}
+         "a = b" | {:a '_.0 :b '_.0}
+         "x : a..b" | {:x (bint) :a (bint) :b (bint)}
+         "c = a - b & a = 5" | {:a (bint) :b (bint) :c (bint)}
+         "c = a - b & c = 5" | {:a (bint) :b  (bint) :c  (bint)}
+         "a - b = c & d:c" | {:a (bset '_.0) :b (bset '_.0) :c (bset '_.0) :d '_.0}
+         "S \\/ {1}" | {:S (bset (bint))}
+         "x = POW(NAT)" | {:x (bset (bset (bint)))}
+         "x : POW(NAT)" | {:x (bset (bint))}
+         "x = id(NAT)" | {:x (brel (bint) (bint))}
+         "x = id(y)" | {:x (brel '_.0 '_.0) :y (bset '_.0)}
+         "not(x=1)" | {:x (bint)}
+         "x = card({1,2})" | {:x (bint)}
+         "x = max({1,2,3,4})" | {:x (bint)}
+         "x : dom(y) & x = FALSE" | {:x (bbool) :y (brel (bbool) '_.0)}
+         "x : ran(y) & x = FALSE" | {:x (bbool) :y (brel '_.0 (bbool))}
+         "x = {1,2} <| NAT * NAT" | {:x (brel (bint) (bint))}
+         "x =  NAT * NAT |> {4}" | {:x (brel (bint) (bint))}
+         "{1|->2}" | {}
+         "x : {1|->2, 3|->5}" | {:x (bpair (bint) (bint))}
+         "x = {1|->1, 2|->3} <+ {a|->b}" | {:x (brel (bint) (bint)) :a (bint) :b (bint)}
+         "x = BOOL <-> INT" | {:x (brel (bbool) (bint))}
+         "x = id(y)[z]" | {:z (bset '_.0) :x  (bset '_.0) :y (bset '_.0)}
+         "x = closure({1|->2,2|->4})"  | {:x (brel (bint) (bint))}
+         "x = iterate({1|->2,2|->4},12)"  | {:x (brel (bint) (bint))}
+         
+         )
+
+(tabular "composed-rules"
+         (fact (tc ?text) => (just ?out))
+         ?text | ?out
+         "x = y & y = z & z : BOOL" | {:x (bbool) :y (bbool) :z (bbool)}
+         "1=1 => y<19" | {:y (bint)}
+         "t = bool(!x.(x:NAT => x:INT))" | {:t (bbool) :x (bint)}
+         "!x.(x:NAT => x:INT)" | {:x (bint)}
+         "x : y & y = POW(STRING)" | {:x (bset (bstring))  :y (bset (bset (bstring)))}
+         "x = y & y + 2 = 5" | {:x (bint) :y (bint)}
+         
+         
+         "d = bool(1<2) => a - b = c & d : c" | {:a (bset (bbool)) :b (bset (bbool)) :c (bset (bbool)) :d (bbool)}
+
+         "a = b \\/ c & d:c & d=12" | {:d (bint) :a (bset (bint)) :b (bset (bint)) :c (bset (bint))}
+         "a = b /\\ c & d:c & d=12" | {:d (bint) :a (bset (bint)) :b
+                                       (bset (bint)) :c (bset (bint))}
+         "s /: NATURAL" | {:s (bint)}
+         "x /= 5" | {:x (bint)}
+         "x = bool(not(1<b))" | {:x (bbool) :b (bint)}
+
+         "x = {1,2}" | {:x (bset (bint))}
+         
+
+         "x = min(y)" | {:x (bint) :y (bset (bint))}
+       
+         "x = NAT * NAT" | {:x (brel (bint) (bint))}
+         "1" | {})
+
+
+
+
 
